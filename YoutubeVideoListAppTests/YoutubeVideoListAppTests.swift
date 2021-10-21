51a491163170a4d3da4e156506d4b9caaa86af8f
akiya <akiya0524@gmail.com>
2021-10-21 06:55:07 -0700

    Initial Commit

---
+++ YoutubeVideoListAppTests/YoutubeVideoListAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  YoutubeVideoListAppTests.swift
+//  YoutubeVideoListAppTests
+//
+//  Created by Akiya Ozawa on R 3/10/21.
+//
+
+import XCTest
+@testable import YoutubeVideoListApp
+
+class YoutubeVideoListAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
